e7cb2a43eadab99b7a4c3380682a1af699f63fd4
Osariusz <osariusz@interia.pl>
2023-04-14 14:49:45 +0200

    Chaos Skeleton generation

--- src/main/java/com/osariusz/event/ModEvents.java
+++ src/main/java/com/osariusz/event/ModEvents.java
@@ -3,6 +3,7 @@ package com.osariusz.event;
 import com.osariusz.chaosskeleton.ChaosSkeletonMod;
 import com.osariusz.chaosskeleton.mobs.ChaosSkeleton;
 import com.osariusz.init.EntityInit;
+import net.minecraft.tags.BiomeTags;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
